d9a2231210e0179f9e3fa955ebbc9fdedc234102
soundarya9597 <119682215+soundarya9597@users.noreply.github.com>
2023-10-11 12:26:26 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,9 +1,9 @@
 def call(ip,creds,warfiles){
                 sshagent(['creds']) {
     // some block
-    sh "scp -o StrictHostKeyChecking=no target/warfile ec2-user@ip:/opt/tomcat9/webapps"
-    sh "ssh ec2-user@ip /opt/tomcat9/bin/shutdown.sh"
-  sh "ssh ec2-user@ip /opt/tomcat9/bin/startup.sh"
+    sh "scp -o StrictHostKeyChecking=no target/${warfile} ec2-user@${ip}:/opt/tomcat9/webapps"
+    sh "ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"
+  sh "ssh ec2-user@${ip} /opt/tomcat9/bin/startup.sh"
     }
 }
 }
