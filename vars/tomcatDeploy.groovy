82ffc86a7e102955dd1fb470875f457bfffe9791
soundarya9597 <119682215+soundarya9597@users.noreply.github.com>
2023-10-11 12:34:33 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -4,8 +4,3 @@ def call(ip,creds,warfiles){
     sh "scp -o StrictHostKeyChecking=no target/${warfile} ec2-user@${ip}:/opt/tomcat9/webapps"
     sh "ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"
   sh "ssh ec2-user@${ip} /opt/tomcat9/bin/startup.sh"
-    }
-}
-}
-}
-}
